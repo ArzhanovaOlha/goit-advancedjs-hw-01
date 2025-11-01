7183a158c7a208e33cb0c266d7c5e97de460a1d6
Olha Arzhanova <arzhanova.olha@gmail.com>
2025-11-01 12:26:22 +0100

    hw-1

---
+++ src/js/2-form.js
@@ -0,0 +1,39 @@
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  try {
+    formData = JSON.parse(savedData);
+    form.email.value = formData.email || '';
+    form.message.value = formData.message || '';
+  } catch (error) {
+    console.error('Error while reading localStorage:', error);
+  }
+}
+
+form.addEventListener('input', e => {
+  const { name, value } = e.target;
+  formData[name] = value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log('Form data submitted:', formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  formData = { email: '', message: '' };
+  form.reset();
+});
---
+++ vite.config.js
@@ -0,0 +1,59 @@
+import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import SortCss from 'postcss-sort-media-queries';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig(({ command }) => ({
+  base: '/goit-advancedjs-hw-01/',
+  define: {
+    global: 'globalThis',
+  },
+  root: 'src',
+  optimizeDeps: {
+    include: ['simplelightbox'],
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+    },
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      input: glob.sync(`${__dirname}/src/*.html`),
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+        entryFileNames: chunkInfo => {
+          if (chunkInfo.name === 'commonHelpers') {
+            return 'commonHelpers.js';
+          }
+          return '[name].js';
+        },
+        assetFileNames: assetInfo => {
+          if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+            return '[name].[ext]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  plugins: [
+    injectHTML(),
+    FullReload(['./src/**/**.html']),
+    SortCss({
+      sort: 'mobile-first',
+    }),
+  ],
+}));
